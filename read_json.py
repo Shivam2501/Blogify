e6e604814ea87d74e474732d8118262ff6bddb4b
Shivam Bharuka <shivam.bharuka@gmail.com>
2016-02-21 04:55:45 -0600

    nltk-filter

--- read_json.py
+++ read_json.py
@@ -15,16 +15,40 @@ pp =pprint.PrettyPrinter(indent=4)
 print("Total number of blog articles: ",len(d))
 
 #pp.pprint(d[0])
+def remove_punctuation(word):
+    return word if str(word) not in string.punctuation else None
 
-print(d[0]['title'])
-print(d[0]['content'])
+def remove_duplicates(words):
+    filter = []
+    for i in words:
+        if i not in filter:
+            filter.append(i)
+    return filter
 
 def tokenize(content):
-    sentences = nltk.sent_tokenize(content)
+    sentences = nltk.word_tokenize(content)
     stopwords = nltk.corpus.stopwords.words()
-    for sentence in sentences:
-        words = map(lambda x: nltk.word_tokenize(sentence.lower()))
+    words = map(lambda x: x if x not in stopwords else None, sentences)
+    words_filtered = []
+    for word in words:
+        words_filtered.append(remove_punctuation(word))
+    words_filtered = map(lambda x: x if len(str(x))>2 else None, words_filtered)
+    words_filtered = filter(None, words_filtered)
+    words_filtered = remove_duplicates(words_filtered)
+    return words_filtered
 
+def frequency(content):
+    words = nltk.word_tokenize(content)
+    freq = nltk.FreqDist(words)
+    return freq
 
+def filter_freq(freq,word):
+    return freq[word]
+
+print(d[0]['title'])
+word_filter = tokenize(d[0]['content'])
+word_freq = frequency(d[0]['content'])
+for word in word_filter:
+    print(word,filter_freq(word_freq,word))
 
 
